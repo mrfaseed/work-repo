e423f820527726cb92e91ca23668f921b2fc98de
mrfaseed <faseedmohamed6@gmail.com>
2025-09-01 18:11:54 +0530

    navbar update v3 by faseed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,13 @@
 import React from "react";
+import "./footer.css"; 
 
-const Footer = () => {
+export default function Footer() {
+  const year = new Date().getFullYear();
+
   return (
-    <footer
-      style={{
-        backgroundColor: "transparent",
-        color: "white",
-        textAlign: "center",
-        padding: "4rem 1rem", 
-        marginTop: "2rem",   
-      }}
-    >
-      <h2>Footer Section</h2>
-      <p>© {new Date().getFullYear()} Hybix. All rights reserved.</p>
+    <footer className="footer">
+  
+      Macha ithula coding yeluthu
     </footer>
   );
-};
-
-export default Footer;
+}
